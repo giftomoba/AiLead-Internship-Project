c9c40486defa86563f729cb8a901b54af73e4c3c
Omeimen Gift Ohiomoba <124467481+giftomoba@users.noreply.github.com>
2023-12-13 12:27:08 +0000

    Add files via upload
    Updated flan t5

--- pdf_tableQueryApp.py
+++ pdf_tableQueryApp.py
@@ -1,17 +1,12 @@
 import os
-from dotenv import load_dotenv
 import camelot
 import streamlit as st
-#from PyPDF2 import PdfReader # Class for reading pdf
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS # Used when creating knowledge base
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI # Initialize the model from OpenAI
-import openai
+import pandas as pd
+from io import StringIO
 
-load_dotenv(dotenv_path=".env")
-openai.api_key = os.getenv("OPENAI_API_KEY")
+from transformers import TapasForQuestionAnswering, AutoTokenizer, TapasTokenizer, TFBartForConditionalGeneration, pipeline
+from transformers import T5ForConditionalGeneration, T5Tokenizer, AutoModelForSeq2SeqLM, AutoTokenizer
+
 
 st.title("PDF Document Table Query App") # Upload PDF and start asking questions on it.
 st.header("Ask Your PDF")
@@ -28,12 +23,12 @@ def extract_tables(file_path):
         st.error(f"Error extracting tables: {str(err)}")
         return None
 
-def display_tables(tables):
-    st.subheader("Extracted Tables:")
-    if tables:
-        for table_num, table in enumerate(tables):
-            st.write(f"Table {table_num + 1}")
-            st.write(table.df)
+def display_tables(tables, selected_tables1):
+    if selected_tables1 is not None:
+        for num in selected_tables1:
+            #if num in range(len(tables)):
+            st.write(f"Table {int(num)}")
+            st.write(tables[int(num) - 1].df)
     else:
         st.write("No tables found in the PDF")
 
@@ -53,41 +48,42 @@ def main():
         # Step 2: PDF parsing and Table Extraction:
         tables = extract_tables(file_path)
         st.success("PDF file successfully uploaded!")
+        st.success(f"I found {len(tables)} tables in your file. Please select the table(s) to query")
 
-        # Display tables in PDF:
-        display_tables(tables)
         # Selecting tables to Query:
         st.sidebar.subheader("Select Tables to Query:")
         selected_tables = st.sidebar.multiselect("Select a table to query:", range(1, len(tables) + 1))
+
+        # Display tables in PDF:
+        st.sidebar.subheader("Display Selected Table(s):")
+        selected_tables1 = st.sidebar.multiselect("Select a table to display:", range(1, len(tables) + 1))
+
+        if selected_tables1:
+            st.subheader("Selected Table(s):")
+            display_tables(tables,selected_tables1)
+
         # Step 3: Geting user question or input
         user_question = st.text_input("Ask a question:")
         upload_button = st.button("Find Answer")
 
         if user_question or upload_button:
-           # Step 4: Preprocessing table data (converting to string)
-            table_text = ""
-            for num, selected_table in enumerate(selected_tables):
-                selected_table_df = tables[selected_table - 1].df
+        # Step 4: Preprocessing table data (converting to string)
+            table_num = selected_tables[0]
+            df = tables[table_num-1].df
+            df_str = df.to_string(index=False, header=False)
 
-                # converting to string:
-                table_text += f'\nTable {num + 1}: {selected_table_df.to_string(index=False)}'
+            # Loading the Flan T5 model and tokenizer:
+            model_name = 'google/flan-t5-base'
+            t5_model = T5ForConditionalGeneration.from_pretrained(model_name)
+            t5_tokenizer = T5Tokenizer.from_pretrained(model_name)
 
-            # Concatenate the user's question with the extracted text from the PDF for LLM:
-            user_question_with_table = f"\n{table_text}\n\n{user_question}"
+            input_text = f"question: {user_question} context: {df_str}"
+            inputs = t5_tokenizer.encode(input_text, return_tensors="pt")
+            outputs = t5_model.generate(inputs)
+            answer = t5_tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-            # Step 5: Utilizing the LLM from GPT-3:
-            # Setting up roles for the system and user:
-            conversation = [
-                {"role": "system", "content": "You are a helpful assistant that can provide information from tables."},
-                {"role": "user", "content": user_question_with_table}]
-            
-            # Use OpenAI GPT-3 to answer the question
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation)
-            
-            # Display the answer
-            answer = response["choices"][0]["message"]["content"]
-            st.success(answer)
-       
+
+            st.success(f"Answer: {answer}")
 
 if __name__ == "__main__":
     main()
